8beedb59b55155866ced67fdbc55acb197e5ead9
helldzl <1949602@qq.com>
2017-12-23 18:39:40 +0800

    core site

--- ch03-hdfs/src/main/java/FileSystemCat.java
+++ ch03-hdfs/src/main/java/FileSystemCat.java
@@ -1,26 +1,28 @@
 // cc FileSystemCat Displays files from a Hadoop filesystem on standard output by using the FileSystem directly
-import java.io.InputStream;
-import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
+import java.io.InputStream;
+import java.net.URI;
+
 // vv FileSystemCat
 public class FileSystemCat {
 
-  public static void main(String[] args) throws Exception {
-    String uri = args[0];
-    Configuration conf = new Configuration();
-    FileSystem fs = FileSystem.get(URI.create(uri), conf);
-    InputStream in = null;
-    try {
-      in = fs.open(new Path(uri));
-      IOUtils.copyBytes(in, System.out, 4096, false);
-    } finally {
-      IOUtils.closeStream(in);
+    public static void main(String[] args) throws Exception {
+        String path = "hdfs://192.168.1.229:9000/user/hadoop/topic-test/model/naiveBayesModel.bin";
+        // String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(path), conf);
+        InputStream in = null;
+        try {
+            in = fs.open(new Path(path));
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
     }
-  }
 }
 // ^^ FileSystemCat
--- ch03-hdfs/src/main/java/URLCat.java
+++ ch03-hdfs/src/main/java/URLCat.java
@@ -1,25 +1,27 @@
 // cc URLCat Displays files from a Hadoop filesystem on standard output using a URLStreamHandler
-import java.io.InputStream;
-import java.net.URL;
 
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.io.IOUtils;
 
+import java.io.InputStream;
+import java.net.URL;
+
 // vv URLCat
 public class URLCat {
 
-  static {
-    URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
-  }
-  
-  public static void main(String[] args) throws Exception {
-    InputStream in = null;
-    try {
-      in = new URL(args[0]).openStream();
-      IOUtils.copyBytes(in, System.out, 4096, false);
-    } finally {
-      IOUtils.closeStream(in);
+    static {
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
     }
-  }
+
+    public static void main(String[] args) throws Exception {
+        String path = "hdfs://192.168.1.229:9000/user/hadoop/topic-test/model/naiveBayesModel.bin";
+        InputStream in = null;
+        try {
+            in = new URL(path).openStream();
+            IOUtils.copyBytes(in, System.out, 4096, false);
+        } finally {
+            IOUtils.closeStream(in);
+        }
+    }
 }
 // ^^ URLCat
